57267ac40cb544b0de2179c9c7ade55a51926945
ivocat <alyrn.n@gmail.com>
2018-05-03 02:23:43 +0300

    Compacted all queries to house from interface into one method

--- interface.rb
+++ interface.rb
@@ -12,8 +12,11 @@ class Interface
   end
 
   def execute
-    get_player_name
-    play_game
+    puts "Enter player's name:"
+    name = gets.chomp.capitalize
+    house.create_player(name)
+    house.add_inteface(self)
+    house.play_game
   end
 
   def show_desk(dealers_bank, dealers_no_of_cards, players_bank, players_cards)
@@ -54,7 +57,7 @@ class Interface
   end
 
   def endgame_prompt
-    if house.player.bank == 0
+    if @house.player.bank == 0
       puts "\nDealer has won the game."
     else
       puts "\nYou have won the game!"
@@ -71,21 +74,10 @@ class Interface
 
   private
 
-  def play_game
-    house.add_inteface(self)
-    house.play_game
-  end
-
   def show_actors_cards(actor)
     actor.hand.each do |card|
       print card.short_name + " "
     end
-  end
-
-  def get_player_name
-    puts "Enter player's name:"
-    name = gets.chomp.capitalize
-    house.create_player(name)
   end
 
   def option_chooser(no_of_options)
