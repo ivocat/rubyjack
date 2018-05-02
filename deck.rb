b3bc925906dffd45e64671b28ee454a6cd7bc48b
ivocat <alyrn.n@gmail.com>
2018-05-02 23:45:05 +0300

    Optimized deck methods

--- deck.rb
+++ deck.rb
@@ -5,7 +5,13 @@ class Deck
 
   def initialize
     @cards = []
-    create_deck
+    suites = %w(♥ ♦ ♣ ♠)
+    ranks = %w(ace 2 3 4 5 6 7 8 9 ten jack queen king)
+    suites.each do |suit|
+      ranks.each do |rank|
+        self.cards << Card.new(rank, suit)
+      end
+    end
     shuffle
   end
 
@@ -15,17 +21,5 @@ class Deck
 
   def deal_top_card
     @cards.pop
-  end
-
-  protected
-
-  def create_deck
-    suites = %w(♥ ♦ ♣ ♠)
-    ranks = %w(ace 2 3 4 5 6 7 8 9 ten jack queen king)
-    suites.each do |suit|
-      ranks.each do |rank|
-        self.cards << Card.new(rank, suit)
-      end
-    end
   end
 end
